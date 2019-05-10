4386f4fdfea55569252d34d729d12bcf5590c4ca
Andy Bettisworth <andy@accreu.com>
2019-05-10 11:54:13 -0500

    Format the backend server

--- server.js
+++ server.js
@@ -1,17 +1,23 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const port = 3333;
+'use strict'
 
-const server = express();
-server.use(bodyParser.json());
-server.use(cors());
+/**
+ * Dependencies
+ */
 
-const sendUserError = (msg, res) => {
-  res.status(422);
-  res.json({ Error: msg });
-  return;
-};
+const express = require('express')
+const body_parser = require('body-parser')
+const cors = require('cors')
+
+/**
+ * Constants
+ */
+
+const port = 3333
+const smurf_id = 1
+
+/**
+ * Define seed data
+ */
 
 let smurfs = [
   {
@@ -20,67 +26,116 @@ let smurfs = [
     age: 200,
     height: '8cm'
   }
-];
-server.get('/smurfs', (req, res) => {
-  res.json(smurfs);
-});
-let smurfId = 1;
+]
 
-server.post('/smurfs', (req, res) => {
-  const { name, age, height } = req.body;
-  const newSmurf = { name, age, height, id: smurfId };
+/**
+ * Define app
+ */
+
+const app = express()
+
+/**
+ * Define error handler
+ */
+
+const send_user_error = (msg, res) => {
+  res.status(422)
+  res.json({ Error: msg })
+  return
+}
+
+/**
+ * Middleware
+ */
+
+app.use(body_parser.json())
+app.use(cors())
+
+/**
+ * Routes
+ */
+
+app.get('/smurfs', (req, res) => {
+  res.json(smurfs)
+})
+
+app.post('/smurfs', (req, res) => {
+  const { name, age, height } = req.body
+  const newSmurf = { name, age, height, id: smurf_id }
   if (!name || !age || !height) {
-    return sendUserError(
+    return send_user_error(
       'Ya gone did smurfed! Name/Age/Height are all required to create a smurf in the smurf DB.',
       res
-    );
+    )
   }
   const findSmurfByName = smurf => {
-    return smurf.name === name;
-  };
+    return smurf.name === name
+  }
   if (smurfs.find(findSmurfByName)) {
-    return sendUserError(
+    return send_user_error(
       `Ya gone did smurfed! ${name} already exists in the smurf DB.`,
       res
-    );
+    )
   }
 
-  smurfs.push(newSmurf);
-  smurfId++;
-  res.json(smurfs);
-});
+  smurfs.push(newSmurf)
+  smurf_id++
+  res.json(smurfs)
+})
 
-server.put('/smurfs/:id', (req, res) => {
-  const { id } = req.params;
-  const { name, age, height } = req.body;
+app.put('/smurfs/:id', (req, res) => {
+  const { id } = req.params
+  const { name, age, height } = req.body
   const findSmurfById = smurf => {
-    return smurf.id == id;
-  };
-  const foundSmurf = smurfs.find(findSmurfById);
-  if (!foundSmurf) {
-    return sendUserError('No Smurf found by that ID', res);
-  } else {
-    if (name) foundSmurf.name = name;
-    if (age) foundSmurf.age = age;
-    if (height) foundSmurf.height = height;
-    res.json(smurfs);
+    return smurf.id == id
   }
-});
+  const foundSmurf = smurfs.find(findSmurfById)
+  if (!foundSmurf) {
+    return send_user_error('No Smurf found by that ID', res)
+  } else {
+    if (name) foundSmurf.name = name
+    if (age) foundSmurf.age = age
+    if (height) foundSmurf.height = height
+    res.json(smurfs)
+  }
+})
 
-server.delete('/smurfs/:id', (req, res) => {
-  const { id } = req.params;
-  const foundSmurf = smurfs.find(smurf => smurf.id == id);
+app.delete('/smurfs/:id', (req, res) => {
+  const { id } = req.params
+  const foundSmurf = smurfs.find(smurf => smurf.id == id)
 
   if (foundSmurf) {
-    const SmurfRemoved = { ...foundSmurf };
-    smurfs = smurfs.filter(smurf => smurf.id != id);
-    res.status(200).json(smurfs);
+    const SmurfRemoved = { ...foundSmurf }
+    smurfs = smurfs.filter(smurf => smurf.id != id)
+    res.status(200).json(smurfs)
   } else {
-    sendUserError('No smurf by that ID exists in the smurf DB', res);
+    send_user_error('No smurf by that ID exists in the smurf DB', res)
   }
-});
+})
 
-server.listen(port, err => {
-  if (err) console.log(err);
-  console.log(`server is listening on port ${port}`);
-});
+/**
+ * Start server
+ */
+
+async function start_server() {
+  /**
+   * Set process title.
+   */
+
+  process.title = 'smurfs'
+
+  app.listen(port, err => {
+    if (err) console.log(err)
+    console.log(`Express app listening on port ${port}`)
+  })
+}
+
+if (module === require.main) {
+  start_server()
+}
+
+/**
+ * Export app
+ */
+
+module.exports = app
